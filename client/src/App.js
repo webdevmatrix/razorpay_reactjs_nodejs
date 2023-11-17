6e185f1315abf70d3c68c7e9e68735484a736b97
rahul-sr <vnsrahulsri@gmail.com>
2023-11-17 14:33:28 +0530

    reactjs integration

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Product from "./Product";
+
+function App() {
+  return (
+    <div className="App">
+      <Product />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Product.js
@@ -0,0 +1,87 @@
+import TshirtImg from "./tshirt.svg";
+
+function Product() {
+  const amount = 500;
+  const currency = "INR";
+  const receiptId = "qwsaq1";
+
+  const paymentHandler = async (e) => {
+    const response = await fetch("http://localhost:5000/order", {
+      method: "POST",
+      body: JSON.stringify({
+        amount,
+        currency,
+        receipt: receiptId,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const order = await response.json();
+    console.log(order);
+
+    var options = {
+      key: "rzp_test_ghTeekIY3ZvfG3", // Enter the Key ID generated from the Dashboard
+      amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+      currency,
+      name: "Acme Corp", //your business name
+      description: "Test Transaction",
+      image: "https://example.com/your_logo",
+      order_id: order.id, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+      handler: async function (response) {
+        const body = {
+          ...response,
+        };
+
+        const validateRes = await fetch(
+          "http://localhost:5000/order/validate",
+          {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const jsonRes = await validateRes.json();
+        console.log(jsonRes);
+      },
+      prefill: {
+        //We recommend using the prefill parameter to auto-fill customer's contact information, especially their phone number
+        name: "Web Dev Matrix", //your customer's name
+        email: "webdevmatrix@example.com",
+        contact: "9000000000", //Provide the customer's phone number for better conversion rates
+      },
+      notes: {
+        address: "Razorpay Corporate Office",
+      },
+      theme: {
+        color: "#3399cc",
+      },
+    };
+    var rzp1 = new window.Razorpay(options);
+    rzp1.on("payment.failed", function (response) {
+      alert(response.error.code);
+      alert(response.error.description);
+      alert(response.error.source);
+      alert(response.error.step);
+      alert(response.error.reason);
+      alert(response.error.metadata.order_id);
+      alert(response.error.metadata.payment_id);
+    });
+    rzp1.open();
+    e.preventDefault();
+  };
+
+  return (
+    <div className="product">
+      <h2>Tshirt</h2>
+      <p>Solid blue cotton Tshirt</p>
+      <img src={TshirtImg} />
+      <br />
+      <button onClick={paymentHandler}>Pay</button>
+    </div>
+  );
+}
+
+export default Product;
